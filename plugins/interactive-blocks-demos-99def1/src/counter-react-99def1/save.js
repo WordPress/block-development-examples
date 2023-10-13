30084cbbe4e5586854f872c52e05a525b0e04b5b
JuanMa Garrido <juanma.garrido@gmail.com>
2023-10-13 13:57:36 +0100

    better styles interactive blocks

--- plugins/interactive-blocks-demos-99def1/src/counter-react-99def1/save.js
+++ plugins/interactive-blocks-demos-99def1/src/counter-react-99def1/save.js
@@ -6,15 +6,25 @@ const Save = ( { attributes } ) => {
 			{ ...useBlockProps.save() }
 			data-gutenberg-attributes={ JSON.stringify( attributes ) }
 		>
-			<span>React Counter</span>
-			<button>+</button>
-			<input
-				width="5"
-				type="number"
-				readOnly
-				value={ attributes.initial }
-			/>
-			<button>-</button>
+			<p>React Counter</p>
+			<details>
+				<summary>Attributes</summary>
+				<pre>
+					<small>{ JSON.stringify( attributes, null, 2 ) }</small>
+				</pre>
+			</details>
+			<br />
+			<div className="counter-container">
+				<button>-</button>
+				<input
+					width="5"
+					type="number"
+					readOnly
+					value={ attributes.initial }
+				/>
+				<button>+</button>
+			</div>
+			
 		</div>
 	);
 };
