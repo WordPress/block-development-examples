8e9f187c2c729900455760da6441037d1b36411b
Ryan Welcher <me@ryanwelcher.com>
2023-10-17 10:34:04 -0400

    Update template to include a non-block example.

--- create-block-template/index.js
+++ create-block-template/index.js
@@ -4,27 +4,20 @@ module.exports = {
 	defaultValues: {
 		namespace: 'gutenberg-examples',
 		example: {},
-		wpScripts: false,
 		customPackageJSON: {
-			scripts: {
-				build: 'wp-scripts build',
-				format: 'wp-scripts format',
-				'lint:css': 'wp-scripts lint-style',
-				'lint:js': 'wp-scripts lint-js',
-				'packages-update': 'wp-scripts packages-update',
-				'plugin-zip': 'wp-scripts plugin-zip',
-				start: 'wp-scripts start',
-			},
-			devDependencies: {
-				'@wordpress/scripts': '^26.12.0',
-			},
 			files: [ '*' ],
 		},
 		customBlockJSON: {
 			keywords: [ 'HEXCODE' ],
 		},
 	},
-	variants: {},
+	variants: {
+		static: {},
+		dynamic: {
+			render: 'file:./render.php',
+		},
+		'non-block': {},
+	},
 	pluginTemplatesPath: join( __dirname, 'templates/plugin' ),
 	blockTemplatesPath: join( __dirname, 'templates/block' ),
 };
