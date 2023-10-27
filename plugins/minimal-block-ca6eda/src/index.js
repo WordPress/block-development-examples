6b851680c20f21ca3abb0f90310257c777c0482e
JuanMa Garrido <juanma.garrido@gmail.com>
2023-10-27 19:01:54 +0100

    better block register recipe

--- plugins/minimal-block-ca6eda/src/index.js
+++ plugins/minimal-block-ca6eda/src/index.js
@@ -1,6 +1,7 @@
 import { registerBlockType } from '@wordpress/blocks';
+import metadata from './block.json';
 
-registerBlockType( 'gutenberg-examples/minimal-block-ca6eda', {
+registerBlockType( metadata.name, {
     edit: function () {
         return <p>Hello World - Block Editor</p>;
     },
