601fb585f2cc0f11822109b4b8599bf42f11eaa4
Ryan Welcher <me@ryanwelcher.com>
2023-11-02 13:51:36 -0400

    Rename template folder

--- templates/block-examples/index.js
+++ templates/block-examples/index.js
@@ -12,8 +12,25 @@ module.exports = {
 					...view.customBlockJSON,
 					keywords: [ hex ],
 				},
+				customPackageJSON: {
+					...view.customPackageJSON,
+					name: `@wp-block-development-examples/${ view.slug }-${ hex }`,
+					scripts: {
+						build: 'wp-scripts build',
+						start: 'wp-scripts start',
+						format: 'wp-scripts format',
+						'lint:css': 'wp-scripts lint-style',
+						'lint:js': 'wp-scripts lint-js',
+						'packages-update': 'wp-scripts packages-update',
+						'plugin-zip': 'wp-scripts plugin-zip',
+					},
+					devDependencies: {
+						'@wordpress/scripts': '^26.15.0',
+					},
+				},
 			};
 		},
+		wpScripts: false,
 		namespace: 'wp-block-development-examples',
 		example: {},
 		customPackageJSON: {
