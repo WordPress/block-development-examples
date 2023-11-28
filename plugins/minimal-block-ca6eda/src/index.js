b2de0521bd9b17f373aea3966ed1fa734014a57e
JuanMa Garrido <juanma.garrido@gmail.com>
2023-11-28 19:25:15 +0000

    new version index.js minimal block

--- plugins/minimal-block-ca6eda/src/index.js
+++ plugins/minimal-block-ca6eda/src/index.js
@@ -3,9 +3,9 @@ import { useBlockProps } from '@wordpress/block-editor';
 import metadata from './block.json';
 
 const Edit = () => <p { ...useBlockProps() }>Hello World - Block Editor</p>;
-const Save = () => <p { ...useBlockProps.save() }>Hello World - Frontend</p>;
+const save = () => <p { ...useBlockProps.save() }>Hello World - Frontend</p>;
 
 registerBlockType( metadata.name, {
 	edit: Edit,
-	save: Save,
+	save,
 } );
