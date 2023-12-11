d8d8a63bd2fdb1f0e35c27c9925be6480794e7db
JuanMa Garrido <juanma.garrido@gmail.com>
2023-12-11 13:35:19 +0000

    Custom blueprint for the countdown block.

--- plugins/interactivity-api-countdown-3cd73e/build/index.asset.php
+++ plugins/interactivity-api-countdown-3cd73e/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'c3ad24404832c0b924f5');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'a30f50362dfe82bca284');
--- plugins/interactivity-api-countdown-3cd73e/build/render.php
+++ plugins/interactivity-api-countdown-3cd73e/build/render.php
@@ -3,22 +3,36 @@ gutenberg_enqueue_module( 'interactivity_api_countdown_3cd73e__view' );
 ?>
 
 <?php
-$currentDateTime = new DateTime();
-$targetDateTime = new DateTime($attributes['date']);
-$timeDifference = $currentDateTime->diff($targetDateTime);
-// Extract the days, hours, minutes, and seconds
-$context   = array(
-	'days' => $timeDifference->d,
-	'hours'  => $timeDifference->h,
-	'minutes'  => $timeDifference->i,
-	'seconds'  => $timeDifference->s,
+/**
+ * PHP file to use when rendering the block type on the server to show on the front end.
+ *
+ * The following variables are exposed to the file:
+ *     $attributes (array): The block attributes.
+ *     $content (string): The block default content.
+ *     $block (WP_Block): The block instance.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ * @package create-block
+ */
+
+
+
+$current_date_time = new DateTime();
+$target_date_time  = new DateTime( $attributes['date'] );
+$time_difference   = $current_date_time->diff( $target_date_time );
+// Extract the days, hours, minutes, and seconds.
+$context = array(
+	'days'    => $time_difference->d,
+	'hours'   => $time_difference->h,
+	'minutes' => $time_difference->i,
+	'seconds' => $time_difference->s,
 );
 ?>
 
 <div 
-	<?php echo get_block_wrapper_attributes(); ?> 
-	data-wp-interactive='{"namespace": "interactivityAPIExamples"}' 
-	data-wp-context='<?php echo wp_json_encode($context); ?>' 
+	<?php echo get_block_wrapper_attributes(); // phpcs:ignore ?> 
+	data-wp-interactive='{"namespace": "interactivity-api-countdown-3cd73e__store"}' 
+	data-wp-context='<?php echo wp_json_encode( $context ); ?>' 
 	data-wp-init="callbacks.startCountdown"
 >
 	<span><span data-wp-text="context.days"></span>D</span>
