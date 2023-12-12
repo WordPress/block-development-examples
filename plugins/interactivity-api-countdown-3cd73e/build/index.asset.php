7bd72ed31acc94bf06c74ba96f7cb018cec1880a
JuanMa Garrido <juanma.garrido@gmail.com>
2023-12-12 17:31:13 +0000

    update example countdown

--- plugins/interactivity-api-countdown-3cd73e/build/index.asset.php
+++ plugins/interactivity-api-countdown-3cd73e/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'a30f50362dfe82bca284');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'a325bb2a9e22fd524565');
