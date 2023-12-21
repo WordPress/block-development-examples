f3202b1175b6e535b1c7dca91d49e926e942f692
JuanMa <juanma.garrido@gmail.com>
2023-12-21 11:14:14 +0100

    Update render.php

--- plugins/block-dynamic-rendering-64756b/src/render.php
+++ plugins/block-dynamic-rendering-64756b/src/render.php
@@ -6,8 +6,17 @@
  *
  * @package block-development-examples
  */
-
 ?>
+
+<?php
+/**
+ * The wp_kses_post function is used to ensure any HTML that is not allowed in a post will be escaped.
+ *
+ * @see https://developer.wordpress.org/reference/functions/wp_kses_post/
+ * @see https://developer.wordpress.org/themes/theme-security/data-sanitization-escaping/#escaping-securing-output
+ */
+?>
+
 <p <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
 	<?php esc_html_e( 'Block with Dynamic Rendering – hello!!!', 'block-development-examples' ); ?>
 </p>
