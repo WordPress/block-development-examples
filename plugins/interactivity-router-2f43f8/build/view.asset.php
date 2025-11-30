127427ec1837b67186d45d22c7e8a7faeca221f0
JuanMa <juanma.garrido@gmail.com>
2025-11-30 09:40:12 +0000

    Update asset versions and enhance styles for interactivity-router plugin. Updated index.asset.php and view.asset.php with new version numbers. Refactored style-index.css to include detailed styles and comments for better clarity and maintainability. Improved source maps for view.js and style-index.css for easier debugging.

--- plugins/interactivity-router-2f43f8/build/view.asset.php
+++ plugins/interactivity-router-2f43f8/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => 'a6894f5f2021fc41944c', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => '7e7c26b8e69522c016bd', 'type' => 'module');
--- plugins/interactivity-router-2f43f8/build/view.js
+++ plugins/interactivity-router-2f43f8/build/view.js
@@ -1,1 +1,92 @@
-import*as e from"@wordpress/interactivity";var r={438:e=>{e.exports=import("@wordpress/interactivity-router")}},t={};function o(e){var i=t[e];if(void 0!==i)return i.exports;var a=t[e]={exports:{}};return r[e](a,a.exports,o),a.exports}o.d=(e,r)=>{for(var t in r)o.o(r,t)&&!o.o(e,t)&&Object.defineProperty(e,t,{enumerable:!0,get:r[t]})},o.o=(e,r)=>Object.prototype.hasOwnProperty.call(e,r);const i=(s={store:()=>e.store},n={},o.d(n,s),n),{state:a}=(0,i.store)("router-2f43f8",{state:{urlRegionDisplay:window.location.href},actions:{*navigate(e){e.preventDefault();const{actions:r}=yield Promise.resolve().then(o.bind(o,438));a.urlRegionDisplay=e.target.href,yield r.navigate(e.target.href)}}});var s,n;
+import * as __WEBPACK_EXTERNAL_MODULE__wordpress_interactivity_8e89b257__ from "@wordpress/interactivity";
+/******/ var __webpack_modules__ = ({
+
+/***/ "@wordpress/interactivity-router":
+/*!**************************************************!*\
+  !*** external "@wordpress/interactivity-router" ***!
+  \**************************************************/
+/***/ ((module) => {
+
+module.exports = import("@wordpress/interactivity-router");;
+
+/***/ }),
+
+/***/ "@wordpress/interactivity":
+/*!*******************************************!*\
+  !*** external "@wordpress/interactivity" ***!
+  \*******************************************/
+/***/ ((module) => {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__wordpress_interactivity_8e89b257__;
+
+/***/ })
+
+/******/ });
+/************************************************************************/
+/******/ // The module cache
+/******/ var __webpack_module_cache__ = {};
+/******/ 
+/******/ // The require function
+/******/ function __webpack_require__(moduleId) {
+/******/ 	// Check if module is in cache
+/******/ 	var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 	if (cachedModule !== undefined) {
+/******/ 		return cachedModule.exports;
+/******/ 	}
+/******/ 	// Create a new module (and put it into the cache)
+/******/ 	var module = __webpack_module_cache__[moduleId] = {
+/******/ 		// no module.id needed
+/******/ 		// no module.loaded needed
+/******/ 		exports: {}
+/******/ 	};
+/******/ 
+/******/ 	// Execute the module function
+/******/ 	__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 
+/******/ 	// Return the exports of the module
+/******/ 	return module.exports;
+/******/ }
+/******/ 
+/************************************************************************/
+/******/ /* webpack/runtime/make namespace object */
+/******/ (() => {
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = (exports) => {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/ })();
+/******/ 
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
+(() => {
+/*!*********************!*\
+  !*** ./src/view.js ***!
+  \*********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _wordpress_interactivity__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @wordpress/interactivity */ "@wordpress/interactivity");
+
+const {
+  state
+} = (0,_wordpress_interactivity__WEBPACK_IMPORTED_MODULE_0__.store)('router-2f43f8', {
+  state: {
+    urlRegionDisplay: window.location.href
+  },
+  actions: {
+    *navigate(e) {
+      e.preventDefault();
+      const {
+        actions
+      } = yield Promise.resolve(/*! import() */).then(__webpack_require__.bind(__webpack_require__, /*! @wordpress/interactivity-router */ "@wordpress/interactivity-router"));
+      state.urlRegionDisplay = e.target.href;
+      yield actions.navigate(e.target.href);
+    }
+  }
+});
+})();
+
+
+//# sourceMappingURL=view.js.map
